cd52e33cc6090503ab9c82b843d4ab67ae8d02a7
jinhu <814030966@qq.com>
2023-04-24 13:14:34 +0800

    Update Mesh.swift

--- Sources/SwiftMesh/Mesh/Mesh.swift
+++ Sources/SwiftMesh/Mesh/Mesh.swift
@@ -57,7 +57,9 @@ extension Mesh{
         return try await requestWithConfig(of : T.self, config: config)
     }
     
-    private func requestWithConfig<T: Decodable>(of type: T.Type, config: Config) async throws -> T {
+    private func requestWithConfig<T: Decodable>(of type: T.Type,
+                                                 modelKeyPath: String? = nil,
+                                                 config: Config) async throws -> T {
         
         guard let url = config.URLString else {
             fatalError("URLString 为空")
@@ -71,7 +73,7 @@ extension Mesh{
                                   encoding: config.requestEncoding,
                                   headers: config.addHeads,
                                   requestModifier: { $0.timeoutInterval = config.timeout }
-        ).serializingDecodable(T.self)
+        ).serializingDecodable(T.self, decoder: JSONDecoder.default)
         
         let result = await requestTask.response.result
         
@@ -130,4 +132,77 @@ extension Mesh{
         }
         return error
     }
+}
+
+public extension JSONDecoder {
+    
+    static let `default`: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .iso8601
+        return decoder
+    }()
+
+    func decode<T>(_ type: T.Type,
+                   from data: Data,
+                   keyPath: String,
+                   keyPathSeparator separator: String = ".") throws -> T where T : Decodable {
+        userInfo[keyPathUserInfoKey] = keyPath.components(separatedBy: separator)
+        return try decode(KeyPathWrapper<T>.self, from: data).object
+    }
+}
+
+private let keyPathUserInfoKey = CodingUserInfoKey(rawValue: "keyPathUserInfoKey")!
+
+private final class KeyPathWrapper<T: Decodable>: Decodable {
+    
+    enum KeyPathError: Error {
+        case `internal`
+    }
+    
+    struct Key: CodingKey {
+        init?(intValue: Int) {
+            self.intValue = intValue
+            stringValue = String(intValue)
+        }
+        
+        init?(stringValue: String) {
+            self.stringValue = stringValue
+            intValue = nil
+        }
+        
+        let intValue: Int?
+        let stringValue: String
+    }
+    
+    typealias KeyedContainer = KeyedDecodingContainer<KeyPathWrapper<T>.Key>
+    
+    init(from decoder: Decoder) throws {
+        guard let keyPath = decoder.userInfo[keyPathUserInfoKey] as? [String],
+              !keyPath.isEmpty
+        else { throw KeyPathError.internal }
+        
+        func getKey(from keyPath: [String]) throws -> Key {
+            guard let first = keyPath.first,
+                  let key = Key(stringValue: first)
+            else { throw KeyPathError.internal }
+            return key
+        }
+        
+        func objectContainer(for keyPath: [String],
+                             in currentContainer: KeyedContainer,
+                             key currentKey: Key) throws -> (KeyedContainer, Key) {
+            guard !keyPath.isEmpty else { return (currentContainer, currentKey) }
+            let container = try currentContainer.nestedContainer(keyedBy: Key.self, forKey: currentKey)
+            let key = try getKey(from: keyPath)
+            return try objectContainer(for: Array(keyPath.dropFirst()), in: container, key: key)
+        }
+        
+        let rootKey = try getKey(from: keyPath)
+        let rooTContainer = try decoder.container(keyedBy: Key.self)
+        let (keyedContainer, key) = try objectContainer(for: Array(keyPath.dropFirst()), in: rooTContainer, key: rootKey)
+        object = try keyedContainer.decode(T.self, forKey: key)
+    }
+    
+    let object: T
 }
