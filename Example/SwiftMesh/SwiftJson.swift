1a26a7dce6b619562490694f2e46af8ab28f5c46
jinhu <814030966@qq.com>
2024-07-11 16:51:09 +0800

    添加一个SwiftyJson扩展

---
+++ Example/SwiftMesh/SwiftJson.swift
@@ -0,0 +1,56 @@
+//
+//  SwiftJson.swift
+//  Example
+//
+//  Created by FunWidget on 2024/7/11.
+//
+
+import Foundation
+import SwiftyJSON
+import Alamofire
+public extension Mesh{
+    func request<T: JSONable>(of type: T.Type) async throws -> T {
+        
+        guard let url = URLString else {
+            fatalError("URLString 为空")
+        }
+        
+        mergeConfig()
+        
+        let request = AF.request(url,
+                                 method: requestMethod,
+                                 parameters: parameters,
+                                 encoding: requestEncoding,
+                                 headers: addHeads,
+                                 interceptor: interceptor,
+                                 requestModifier: { $0.timeoutInterval = self.timeout }
+        )
+        
+        return try await handleModel(of: type, request: request)
+    }
+    
+    
+    func handleModel<T: JSONable>(of type: T.Type,
+                                  request: DataRequest) async throws -> T {
+        
+        let requestTask = request.serializingData()
+        
+        let result = await requestTask.response.result
+        
+        return try await withCheckedThrowingContinuation { continuation in
+            switch result{
+            case .success(let data):
+                
+                if let json = try? JSON(data: data){
+                    let model = T.init(json: json)
+                    continuation.resume(returning: model)
+                }else{
+                    continuation.resume(throwing: APIError.jsonError(reason: "Json 解析失败"))
+                }
+            case .failure(let error):
+                
+                continuation.resume(throwing: self.handleError(error: error))
+            }
+        }
+    }
+}
